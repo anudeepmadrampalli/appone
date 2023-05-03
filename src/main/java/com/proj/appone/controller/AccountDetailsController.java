4000cf1f8ceb4e52a03a64babd937eefc27a330c
Anudeep <anudeep571@gmail.com>
2023-05-04 00:24:05 +1000

    Create accounts API added.

--- src/main/java/com/proj/appone/controller/AccountDetailsController.java
+++ src/main/java/com/proj/appone/controller/AccountDetailsController.java
@@ -2,14 +2,19 @@ package com.proj.appone.controller;
 
 import com.proj.appone.entities.AccountDetails;
 import com.proj.appone.service.AccountDetailsServiceImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 public class AccountDetailsController {
 
     @Autowired
@@ -23,5 +28,13 @@ public class AccountDetailsController {
     @GetMapping("/account")
     public AccountDetails getAccount(@RequestHeader Long accountNumber){
         return accountDetailsService.getAccount(accountNumber);
+    }
+
+    @PutMapping("/init")
+    public ResponseEntity<HttpStatus> createAccount(){
+        log.info("*****Creating accounts*****");
+        accountDetailsService.createAccounts();
+        log.info("*****Accounts Created*****");
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
